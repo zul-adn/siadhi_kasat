0abf8c61cffb35d4f61856d98ce7ba36624be6a5
zul-adn <zbojetx@gmail.com>
2021-07-08 01:09:43 +0700

    add device detect

--- src/components/rootapp.js
+++ src/components/rootapp.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { connect } from "react-redux";
-
+import { isDesktop } from "react-device-detect";
 import Questionpage from './pages/Questionpage';
 
 function Rootapp() {
     return (
         <div>
-            <Questionpage />
+            {!isDesktop ?
+                <div style={{  height: '100vh', display:'flex', justifyContent:'center', flexDirection:'column', alignItems:'center', padding: 80, textAlign:'center'}}>
+                    <img src='https://image.flaticon.com/icons/png/512/2659/2659980.png' style={{ width:'50%', marginBottom:100 }} />
+                    Mohon Maaf, untuk sementara Siadhi hanya support untuk penggunaan berbasis dekstop, Terima Kasih
+                </div>
+                :
+                <Questionpage />
+            }
+
         </div>
     )
 }
 
 const mapStateToProps = ({ app }) => {
     return {
-       
+
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-       
+
     }
 };
 
